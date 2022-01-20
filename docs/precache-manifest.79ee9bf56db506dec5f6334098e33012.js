f15509fc660df2e7155a4f73a59c552bfb049445
edv4ldez <edwin.valdez@wizeline.com>
2022-01-19 22:35:28 -0600

    Updating page

---
+++ docs/precache-manifest.79ee9bf56db506dec5f6334098e33012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e09cf722d7105deeab6c49e2970663e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ed030ea24d29fc411a7",
+    "url": "/static/css/main.d5d24586.chunk.css"
+  },
+  {
+    "revision": "f789bee9bec051629a58",
+    "url": "/static/js/2.52387271.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.52387271.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed030ea24d29fc411a7",
+    "url": "/static/js/main.8917fbc4.chunk.js"
+  },
+  {
+    "revision": "0d4e3fbca24db45e5036",
+    "url": "/static/js/runtime-main.86719c39.js"
+  }
+]);
